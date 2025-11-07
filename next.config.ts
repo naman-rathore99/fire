da7c95fca1b0957d8d0f7ee9a9a52548c8f2a9c0
naman-rathore99 <157107543+naman-rathore99@users.noreply.github.com>
2025-11-07 22:01:00 +0530

    Remove ESLint ignoreDuringBuilds configuration
    Removed ESLint configuration to ignore during builds.

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-    eslint: {
-    ignoreDuringBuilds: true, // This disables ESLint for all folders during production builds
-  },
+   
 };
 
 export default nextConfig;
